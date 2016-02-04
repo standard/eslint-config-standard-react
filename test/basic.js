c0150518c7b2585e319fb2b8ddd4afbace236a5a
Feross Aboukhadijeh <feross@feross.org>
2016-02-04 01:41:37 -0800

    BREAKING: require eslint 2.0.0

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,7 @@ var config = require('../')
 var test = require('tape')
 
 test('test basic properties of config', function (t) {
-  t.ok(isObject(config.ecmaFeatures))
+  t.ok(isObject(config.parserOptions))
   t.ok(isObject(config.rules))
   t.ok(Array.isArray(config.plugins))
   t.end()
