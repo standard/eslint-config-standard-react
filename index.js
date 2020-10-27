ae075b3a5f7d46280c77fd396114bb04ba887367
Feross Aboukhadijeh <feross@feross.org>
2020-10-27 12:36:34 -1000

    package metadata

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*! eslint-config-standard-react. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 module.exports = Object.assign({}, require('./eslintrc.json'), {
   extends: [ require.resolve('eslint-config-standard-jsx') ]
 })
