3537f5f02bccf022bca74afb953e67fbab607b8c
Feross Aboukhadijeh <feross@feross.org>
2020-10-29 10:21:02 -1000

    remove reference to eslint-config-standard-jsx

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 /*! eslint-config-standard-react. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
-module.exports = Object.assign({}, require('./eslintrc.json'), {
-  extends: [require.resolve('eslint-config-standard-jsx')]
-})
+module.exports = require('./eslintrc.json')
