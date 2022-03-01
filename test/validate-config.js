78895eeb0f29794552ed6146dcc1f5a79e768ce0
Katerina Koukiou <kkoukiou@redhat.com>
2022-03-01 16:06:51 +0100

    feat: support ESLint 8.x (#69)

--- test/validate-config.js
+++ test/validate-config.js
@@ -1,16 +1,17 @@
-const eslint = require('eslint')
+const { ESLint } = require('eslint')
 const test = require('tape')
 
-test('load config in eslint to validate all rule syntax is correct', function (t) {
-  const CLIEngine = eslint.CLIEngine
-
-  const cli = new CLIEngine({
-    useEslintrc: false,
-    configFile: 'eslintrc.json'
+test('load config in eslint to validate all rule syntax is correct', async t => {
+  const cli = new ESLint({
+      useEslintrc: false,
+      overrideConfigFile: 'eslintrc.json'
   })
 
-  const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
 
-  t.equal(cli.executeOnText(code).errorCount, 0)
+  const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+  const result = await cli.lintText(code)
+
+  t.equal(result.length, 1, 'Should return a single result item')
+  t.equal(result[0].errorCount, 0, 'The result item should show no errors')
   t.end()
 })
